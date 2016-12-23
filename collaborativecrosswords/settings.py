80c3a240a18f9cd47f7b0cc23fa718c7b02a9d72
Logan Su <ludi1001@gmail.com>
2016-12-22 17:36:27 -0800

    Added static home page.

--- collaborativecrosswords/settings.py
+++ collaborativecrosswords/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'collaborativecrosswords.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_ROOT, 'templates/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# Append slashes to URLs.
+APPEND_SLASH = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
